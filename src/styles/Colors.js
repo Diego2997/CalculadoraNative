ce0665000dfc610b324172a275a01d53de1b5427
Diego Mercado <diego.mercado1997@hotmail.com>
2022-11-11 21:28:37 -0300

    first commit

---
+++ src/styles/Colors.js
@@ -0,0 +1,11 @@
+export const myColors = {
+  light: "#f1f2f3",
+  dark: "#17171c",
+  blue: "#485efc",
+  btnGray: "#4e585f",
+  btnDark: "#2e2f38",
+  grey: "#747477",
+  black: "#000000",
+  white: "#ffffff",
+  result: "#460582",
+};
